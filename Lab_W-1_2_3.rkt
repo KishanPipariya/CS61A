5537185b746d57a9f76ead49d6292b2efaba9ba3
Kishan Pipariya <intelkishan@gmail.com>
2022-06-28 20:57:21 +0530

    Lab_W-1_2_1 is added

--- Lab_W-1_2_3.rkt
+++ Lab_W-1_2_3.rkt
@@ -1,1 +1,12 @@
 #lang simply-scheme
+
+(define (square x) (* x x))
+
+(define (f a b c)
+  (if
+    (> a b) (if (> b c) (+ (square a) (square b))
+                (+ (square a) (square c)))
+    (if (> a c) (+ (square a) (square b))
+        (+ (square b) (square c)))))
+
+(f 4 2 3)
