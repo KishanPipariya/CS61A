37da251c1f581bc72af1019643d7e61ac45a9b8b
Kishan Pipariya <intelkishan@gmail.com>
2022-06-29 13:41:50 +0530

    Q1_7 is added

--- Q1_7.rkt
+++ Q1_7.rkt
@@ -1,19 +1,16 @@
 #lang sicp
-(define (new-if predicate then-clause else-clause)
-  (cond (predicate then-clause)
-        (else else-clause)))
 
-(define (average x y) (/ (+ x y) 2))
+(define (abs x)
+  (if (< x 0) (- x) x))
 
-(define (square x) (* x x))
+(define (good-enough? guess x)
+  (if (< (abs (- (* guess guess guess) x)) 0.01) #t #f))
 
-(define (abs x)(if (>= x 0) x (- x)))
+(define (improve-guess guess x)
+  (/ (+ (/ x (* guess guess)) (* 2 guess)) 3))
 
-(define (good-enough? guess prev-guess) (< ( abs (/ (- guess prev-guess) guess)) 0.01))
+(define (cb-iter guess x)
+  (if (good-enough? guess x) (guess)
+      (cb-iter (improve-guess guess x) x)))
 
-(define (improve guess x) (average guess (/ x guess)))
-
-(define (sqrt-iter guess prev-guess x) (if (good-enough? guess prev-guess) guess (sqrt-iter (improve guess x) guess x)))
-
-(define (sqrt x) (sqrt-iter 1.0 100.0 x))
-(sqrt 100000000000)
+(cb-iter 1 27)
